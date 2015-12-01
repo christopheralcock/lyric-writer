6e49086108283d8d3bcb9e73a817cdad11b7a884
Christopher Alcock <christopheralcock@Christophers-MacBook-Pro-2.local>
2015-12-01 22:17:39 +0000

    uploading for slack overflow

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,15 @@
+require File.join(File.dirname(__FILE__), '..', 'app/lyric-writer.rb')
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+require 'capybara/cucumber'
+
+Capybara.app = LyricWriter
+
 
 RSpec.configure do |config|
+
+  include Capybara::DSL
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -11,7 +18,5 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
-
-  config.include Capybara::DSL
 
 end
