f947ea1de0cf5213a3c5cba55c20c9022daa949f
Christopher Alcock <christopheralcock@Christophers-MacBook-Pro-2.local>
2016-02-13 23:40:16 +0000

    spiking the mechanics with cookies

--- app/lyric-writer.rb
+++ app/lyric-writer.rb
@@ -1,20 +1,72 @@
 require 'sinatra/base'
-require 'sinatra/flash'
+require 'sinatra/cookies'
 require 'rubygems'
 require_relative 'models/phrase'
+require 'json'
+require 'uri'
+require_relative 'models/hash'
 
 class LyricWriter < Sinatra::Base
+  helpers Sinatra::Cookies
+
 
   get '/' do
-    @phrases = Phrase.all
+    cookies[:dictionary] ||= JSON.dump({})
+    @dictionary = JSON.parse(URI.decode(cookies[:dictionary]))
+    create_haiku
     erb :index
   end
 
-  post '/phrases' do
-    Phrase.create(phrase: params[:phrase], syllables: params[:syllables], stress_pattern: params[:stress_pattern])
-    redirect to('/')
+  get '/cookies' do
+    cookies[:dictionary] = JSON.dump({})
+    "cookies cleared"
   end
+
+  post '/' do
+    cookies[:dictionary] ||= JSON.dump({})
+    parsed = JSON.parse(URI.decode(cookies[:dictionary]))
+    p "parsed object: #{parsed}"
+    phrase = params[:phrase]
+    syllables = params[:syllables].to_i
+    parsed[phrase] = syllables
+    p "parsed object after adding page contents: #{parsed}"
+    cookies[:dictionary] = JSON.dump(parsed)
+    redirect '/'
+  end
+
 
     # start the server if ruby file executed directly
   run! if app_file == $0
+
+
+  helpers do
+
+    def sampler(line_length)
+      @sample_line = ""
+      sample_line_array = []
+      sample_line_length = line_length
+      @dictionary.shuffle!
+      @dictionary.each do |phrase, syllables|
+        if sample_line_length >= syllables
+          sample_line_array << phrase
+          sample_line_length -= syllables
+        end
+      end
+      sample_line_array.each do |word|
+        @sample_line += " #{word}"
+      end
+      @sample_line
+    end
+
+    def create_haiku
+      @haiku = []
+      @haiku << sampler(5)
+      @haiku << sampler(7)
+      @haiku << sampler(5)
+
+    end
+
+
+  end
+
 end
--- app/models/phrase.rb
+++ app/models/phrase.rb
@@ -1,18 +1,3 @@
-require 'data_mapper'
-require 'dm-postgres-adapter'
-
 class Phrase
 
-  include DataMapper::Resource
-
-  property :id,             Serial
-  property :phrase,         String
-  property :syllables,      Integer
-  property :stress_pattern, String
-
-
 end
-
-DataMapper.setup(:default, "postgres://localhost/lyrics_writer_test")
-DataMapper.finalize
-DataMapper.auto_upgrade!
